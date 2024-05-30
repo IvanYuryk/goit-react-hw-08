8dfec7f5f00490ebb95022e16888471ae571c2e5
IvanYuryk <143449289+IvanYuryk@users.noreply.github.com>
2024-05-30 22:01:36 +0200

    update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,6 +3,8 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useDispatch } from "react-redux";
 import { addContact } from "../../redux/contacts/operations.js";
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
 
 import { TiContacts } from "react-icons/ti";
 import { RiContactsLine } from "react-icons/ri";
@@ -41,42 +43,46 @@ const ContactForm = () => {
       }}
       onSubmit={handleSubmit}
     >
-      <Form className={css.box}>
-        <div className={css.fieldBox}>
-          <label className={css.label} htmlFor={nameFieldId}>
-            <RiContactsLine className={css.icon} />
-            Name
-          </label>
-          <Field
-            className={css.field}
-            type="text"
-            name="name"
-            placeholder="Jacob Mercer"
-            id={nameFieldId}
-          />
-          <ErrorMessage className={css.error} name="name" component="span" />
-        </div>
+      {({ setFieldValue, values }) => (
+        <Form className={css.box}>
+          <div className={css.fieldBox}>
+            <label className={css.label} htmlFor={nameFieldId}>
+              <RiContactsLine className={css.icon} />
+              Name
+            </label>
+            <Field
+              className={css.field}
+              type="text"
+              name="name"
+              placeholder="Jacob Mercer"
+              id={nameFieldId}
+            />
+            <ErrorMessage className={css.error} name="name" component="span" />
+          </div>
 
-        <div className={css.fieldBox}>
-          <label className={css.label} htmlFor={numberFieldId}>
-            <FiPhone className={css.icon} />
-            Number
-          </label>
-          <Field
-            className={css.field}
-            type="number"
-            name="number"
-            placeholder="012-345-67-89"
-            id={numberFieldId}
-          />
-          <ErrorMessage className={css.error} name="number" component="span" />
-        </div>
+          <div className={css.fieldBox}>
+            <label className={css.label} htmlFor={numberFieldId}>
+              <FiPhone className={css.icon} />
+              Number
+            </label>
+            <PhoneInput
+              country={'us'}
+              value={values.number}
+              onChange={value => setFieldValue('number', value)}
+              inputClass={css.phoneInput}
+              containerClass={css.phoneInputContainer}
+              buttonClass={css.phoneInputButton}
+              dropdownClass={css.phoneInputDropdown}
+            />
+            <ErrorMessage className={css.error} name="number" component="span" />
+          </div>
 
-        <button className={css.btn} type="submit">
-          Add Contact
-          <TiContacts className={css.addIcon} />
-        </button>
-      </Form>
+          <button className={css.btn} type="submit">
+            Add Contact
+            <TiContacts className={css.addIcon} />
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
